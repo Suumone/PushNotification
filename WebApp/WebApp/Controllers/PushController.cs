a7d3c3eef5cb7c686062ff319788c36ea8537c46
Suumone <42411293+Suumone@users.noreply.github.com>
2018-12-22 04:03:29 +0300

    Add files via upload

---
+++ WebApp/WebApp/Controllers/PushController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PushSender.Controllers
+{
+    [Route("api/[controller]")]
+    public class PushController : Controller
+    {
+
+        [HttpPost]
+        public void Post(string appToken, string userKey, string message)
+        {
+            var parameters = new NameValueCollection {
+                { "token", appToken },
+                { "user", userKey },
+                { "message", message }
+            };
+
+            using (var client = new WebClient())
+            {
+                client.UploadValues("https://api.pushover.net/1/messages.json", parameters);
+            }
+        }
+    }
+}
